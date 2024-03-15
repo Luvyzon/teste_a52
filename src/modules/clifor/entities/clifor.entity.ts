df657718616dbdf09c3db24168de7d4a178db214
Luvison <matheusluvison18@outlook.com>
2024-03-15 20:37:20 -0300

    initializing clifor entity

--- src/modules/clifor/entities/clifor.entity.ts
+++ src/modules/clifor/entities/clifor.entity.ts
@@ -1,1 +1,40 @@
-export class Clifor {}
+import {
+  PrimaryGeneratedColumn,
+  Column,
+  Entity,
+  CreateDateColumn,
+  UpdateDateColumn,
+  BeforeUpdate,
+} from 'typeorm';
+
+@Entity({ name: 'clifors' })
+export class Clifor {
+  @PrimaryGeneratedColumn()
+  id: number;
+  @Column()
+  nome: string;
+  @Column()
+  cpf_cnpj: number;
+  @Column()
+  contato: string;
+  @Column()
+  cep: number;
+  @Column()
+  endereco: string;
+  @Column()
+  cidade: string;
+  @Column()
+  estado: number;
+  @Column()
+  cli_for: number;
+  @Column()
+  ativo: number;
+  @CreateDateColumn()
+  public created_at: Date;
+  @UpdateDateColumn()
+  public updated_at: Date;
+  @BeforeUpdate()
+  updateDates() {
+    this.updated_at = new Date();
+  }
+}
