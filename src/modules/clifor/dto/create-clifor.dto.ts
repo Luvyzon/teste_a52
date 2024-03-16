46b1548493620e1b8d06c61f3b954fa99fddae27
Luvison <matheusluvison18@outlook.com>
2024-03-16 08:20:41 -0300

    init clifor dto

--- src/modules/clifor/dto/create-clifor.dto.ts
+++ src/modules/clifor/dto/create-clifor.dto.ts
@@ -1,1 +1,11 @@
-export class CreateCliforDto {}
+export class CreateCliforDto {
+  nome: string;
+  cpf_cnpj: number;
+  contato: string;
+  cep: number;
+  endereco: string;
+  cidade: string;
+  estado: number;
+  cli_for: number;
+  ativo: number;
+}
